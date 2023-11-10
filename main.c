3ede3778d890ef8979eae1521f32942f12160b0e
Maridiane Lugaresi <maridiane.lugaresi@hotmail.com>
2023-11-09 22:18:12 -0300

    Adicionada estrutura para inicializar entrelacamento transacoes

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
+typedef struct historia
+{
+    char operacao[10];
+    int transacao;
+} tHistoria;
+
 
 int main(){
+
+    tHistoria historia[] = {{'ls[x]',1},
+                            {'r[x]',1}, 
+                            {'ls[x]',2},
+                            {'r[x]',2},
+                            {'lx[y]',1},
+                            {'w[y]',1},
+                            {'c',1},
+                            {'ux[y]',1},
+                            {'us[x]',1},
+                            {'lx[y]',2},
+                            {'w[y]',2},
+                            {'lx[x]',2},
+                            {'w[x]',2},
+                            {'c',2},
+                            {'ux[x]',2},
+                            {'ux[y]',2}};
+    
     return 0;
+
 }

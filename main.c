2fd8c304e3fea87267f6d54d1f17489412eaf2f9
augustofklein <augusto_klein@hotmail.com>
2023-11-11 22:23:34 -0300

    Adicao de variaveis para iniciar historia

--- main.c
+++ main.c
@@ -1,48 +1,63 @@
 #include<stdio.h>
 
+int QTD_HISTORIA = 16;
+
 int TRANSACAO1 = 1;
 int TRANSACAO2 = 2;
 
-int COMMIT                      = 1;  // c
-int LEITURA_X                   = 2;  // r[x]
-int LEITURA_Y                   = 3;  // r[y]
-int ESCRITA_X                   = 4;  // w[x]
-int ESCRITA_Y                   = 5;  // w[y]
-int BLOQUEIO_COMPARTILHADO_X    = 6;  // ls[x] 
-int BLOQUEIO_COMPARTILHADO_Y    = 7;  // ls[y]
-int BLOQUEIO_EXCLUSIVO_X        = 8;  // lx[x]
-int BLOQUEIO_EXCLUSIVO_Y        = 9;  // lx[y]
-int DESBLOQUEIO_EXCLUSIVO_X     = 10; // ux[x]
-int DESBLOQUEIO_EXCLUSIVO_Y     = 11; // ux[y]
-int DESBLOQUEIO_COMPARTILHADO_X = 12; // us[x]
-int DESBLOQUEIO_COMPARTILHADO_Y = 13; // us[y]
+int COMMIT                    = 1; // c
+int LEITURA                   = 2; // r
+int ESCRITA                   = 3; // w
+int BLOQUEIO_COMPARTILHADO    = 4; // ls
+int BLOQUEIO_EXCLUSIVO        = 5; // lx
+int DESBLOQUEIO_EXCLUSIVO     = 6; // ux
+int DESBLOQUEIO_COMPARTILHADO = 7; // us
+
+char VARIAVEL_X = 'x';
+char VARIAVEL_Y = 'y';
 
 typedef struct historia
 {
     int operacao;
+    char variavel;
     int transacao;
 } tHistoria;
 
+void enviaOperacaoEscalonador(tHistoria historia){
+
+}
+
+void processaEscalonamentoDados(tHistoria historia[QTD_HISTORIA]){
+
+    int i;
+
+    for(i=0;i<QTD_HISTORIA;i++){
+        enviaOperacaoEscalonador(historia[i]);
+    }
+
+}
 
 int main(){
 
     // HI = ls[x] r[x] ls[x] r[x] lx[y] w[y] c ux[y] us[x] lx[y] w[y] lx[x] w[x] c ux[x] ux[y]
-    tHistoria historia[] = {{BLOQUEIO_COMPARTILHADO_X,    TRANSACAO1},
-                            {LEITURA_X,                   TRANSACAO1}, 
-                            {BLOQUEIO_COMPARTILHADO_X,    TRANSACAO2},
-                            {LEITURA_X,                   TRANSACAO2},
-                            {BLOQUEIO_EXCLUSIVO_Y,        TRANSACAO1},
-                            {ESCRITA_Y,                   TRANSACAO1},
-                            {COMMIT,                      TRANSACAO1},
-                            {DESBLOQUEIO_EXCLUSIVO_Y,     TRANSACAO1},
-                            {DESBLOQUEIO_COMPARTILHADO_X, TRANSACAO1},
-                            {BLOQUEIO_EXCLUSIVO_Y,        TRANSACAO2},
-                            {ESCRITA_Y,                   TRANSACAO2},
-                            {BLOQUEIO_EXCLUSIVO_X,        TRANSACAO2},
-                            {ESCRITA_X,                   TRANSACAO2},
-                            {COMMIT,                      TRANSACAO2},
-                            {DESBLOQUEIO_EXCLUSIVO_X,     TRANSACAO2},
-                            {DESBLOQUEIO_EXCLUSIVO_Y,     TRANSACAO2}};
+    tHistoria historia[] = {{BLOQUEIO_COMPARTILHADO,    VARIAVEL_X, TRANSACAO1},
+                            {LEITURA,                   VARIAVEL_X, TRANSACAO1}, 
+                            {BLOQUEIO_COMPARTILHADO,    VARIAVEL_X, TRANSACAO2},
+                            {LEITURA,                   VARIAVEL_X, TRANSACAO2},
+                            {BLOQUEIO_EXCLUSIVO,        VARIAVEL_Y, TRANSACAO1},
+                            {ESCRITA,                   VARIAVEL_Y, TRANSACAO1},
+                            {COMMIT,                    'c',        TRANSACAO1},
+                            {DESBLOQUEIO_EXCLUSIVO,     VARIAVEL_Y, TRANSACAO1},
+                            {DESBLOQUEIO_COMPARTILHADO, VARIAVEL_X, TRANSACAO1},
+                            {BLOQUEIO_EXCLUSIVO,        VARIAVEL_Y, TRANSACAO2},
+                            {ESCRITA,                   VARIAVEL_Y, TRANSACAO2},
+                            {BLOQUEIO_EXCLUSIVO,        VARIAVEL_X, TRANSACAO2},
+                            {ESCRITA,                   VARIAVEL_X, TRANSACAO2},
+                            {COMMIT,                    'c',        TRANSACAO2},
+                            {DESBLOQUEIO_EXCLUSIVO,     VARIAVEL_X, TRANSACAO2},
+                            {DESBLOQUEIO_EXCLUSIVO,     VARIAVEL_Y, TRANSACAO2}};
+
+    processaEscalonamentoDados(historia);
     
     return 0;
 
